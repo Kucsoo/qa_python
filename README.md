f0135b30dd6d9dffe9d4b5c28cfc9291bbff578d
Ekaterina Bogdanova <kucsoo@gmail.com>
2023-04-12 21:59:52 +0300

    Added new tests scripts

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # qa_python
+###1) test_add_new_book_add_two_books 
+CCan add 2 book 
+###2) test_add_new_book_add_same_book 
+Cannot add the same book twice книгу дважды
+###3) test_set_book_rating_more_than_11 
+Cannot ser rating more than 10  больше 10
+###4) test_set_book_rating_less_than_1 
+Cannot set rating less than 1� меньше 1
+###5) test_get_book_rating_not_in_list
+Book not in list doesn-t have rating нет рейтинга
+###6) test_add_book_in_favorites_add_one_book
+Add book to favourites в избранное
+###7) test_delete_book_from_favorites_del_one_book
+Delete one book from favourites� избранного
+###8) test_add_book_in_favorites_not_in_books_rating
+Cannot add book to favorites if it not in books_rating
+###9) test_get_books_with_specific_rating_book_not_such_rating 
+Cannot get list if book not with specific ratingинг не совпадает
+
+
+
+
+
--- tests.py
+++ tests.py
@@ -22,3 +22,51 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    def test_add_new_book_add_same_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Тень и кость')
+        collector.add_new_book('Тень и кость')
+        assert len(collector.get_books_rating()) == 1
+
+    def test_set_book_rating_more_than_11(self):
+        collector = BooksCollector()
+        collector.add_new_book('Автостопом по галактике')
+        collector.set_book_rating ('Автостопом по галактике', 12)
+        assert collector.books_rating['Автостопом по галактике'] == 1
+
+    def test_set_book_rating_less_than_1(self):
+        collector = BooksCollector()
+        collector.add_new_book('Свинка Пеппа')
+        collector.set_book_rating('Свинка Пеппа', 0.5)
+        assert collector.books_rating['Свинка Пеппа'] == 1
+
+    def test_get_book_rating_not_in_list(self):
+        collector = BooksCollector()
+        collector.get_book_rating('Книга Келлс')
+        assert collector.books_rating.get('Книга Келлс') == None
+
+    def test_add_book_in_favorites_add_one_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Тень и кость')
+        collector.add_new_book('Шестерка воронов')
+        collector.add_book_in_favorites('Тень и кость')
+        assert 'Тень и кость' in collector.get_list_of_favorites_books()
+
+    def test_delete_book_from_favorites_del_one_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Шестерка воронов')
+        collector.add_book_in_favorites('Шестерка Воронов')
+        collector.delete_book_from_favorites('Шестерка Воронов')
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+    def test_add_book_in_favorites_not_in_books_rating(self):
+        collector = BooksCollector()
+        collector.add_book_in_favorites('Разум и чувства')
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+    def test_get_books_with_specific_rating_book_not_such_rating(self):
+        collector = BooksCollector()
+        collector.add_new_book('Тень и кость')
+        collector.get_books_with_specific_rating(10)
+        assert len(collector.get_books_with_specific_rating(10))== 0
