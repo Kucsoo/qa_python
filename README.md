597798bdbc7fcd17828b9bf4d427992144ab17b8
Ekaterina Bogdanova <kucsoo@gmail.com>
2023-04-13 15:50:41 +0300

    Added 2 new tests scripts, add assert message

--- README.md
+++ README.md
@@ -1,22 +1,26 @@
 # qa_python
 ###1) test_add_new_book_add_two_books 
-CCan add 2 book 
+Can add 2 book 
 ###2) test_add_new_book_add_same_book 
-Cannot add the same book twice книгу дважды
+Cannot add the same book twice ажды
 ###3) test_set_book_rating_more_than_11 
-Cannot ser rating more than 10  больше 10
-###4) test_set_book_rating_less_than_1 
-Cannot set rating less than 1� меньше 1
+Cannot ser rating more than 10 
+###4) test_set_book_rating_7
+set rating 7 to book
+###5) test_set_book_rating_less_than_1 
+Cannot set rating less than n 1
 ###5) test_get_book_rating_not_in_list
-Book not in list doesn-t have rating нет рейтинга
-###6) test_add_book_in_favorites_add_one_book
-Add book to favourites в избранное
-###7) test_delete_book_from_favorites_del_one_book
-Delete one book from favourites� избранного
-###8) test_add_book_in_favorites_not_in_books_rating
+Book not in list doesn-t have rating инга
+###7) test_add_book_in_favorites_add_one_book
+Add book to favourites �нное
+###8) test_delete_book_from_favorites_del_one_book
+Delete one book from favouritesного
+###9) test_add_book_in_favorites_not_in_books_rating
 Cannot add book to favorites if it not in books_rating
-###9) test_get_books_with_specific_rating_book_not_such_rating 
-Cannot get list if book not with specific ratingинг не совпадает
+###10) test_get_books_with_specific_rating_book_with_rating_8
+Can get book with rating 8 
+###11) test_get_books_with_specific_rating_book_not_such_rating 
+Cannot get list if book not with specific rating�адает
 
 
 
--- tests.py
+++ tests.py
@@ -18,7 +18,7 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_rating()) == 2, "Должно добавится 2 книги"
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
@@ -27,46 +27,59 @@ class TestBooksCollector:
         collector = BooksCollector()
         collector.add_new_book('Тень и кость')
         collector.add_new_book('Тень и кость')
-        assert len(collector.get_books_rating()) == 1
+        assert len(collector.get_books_rating()) == 1, "Должна обавится только 1 книга"
 
     def test_set_book_rating_more_than_11(self):
         collector = BooksCollector()
         collector.add_new_book('Автостопом по галактике')
         collector.set_book_rating ('Автостопом по галактике', 12)
-        assert collector.books_rating['Автостопом по галактике'] == 1
+        assert collector.get_book_rating('Автостопом по галактике') == 1, "Рейтинг книги должен быть 1"
 
     def test_set_book_rating_less_than_1(self):
         collector = BooksCollector()
         collector.add_new_book('Свинка Пеппа')
         collector.set_book_rating('Свинка Пеппа', 0.5)
-        assert collector.books_rating['Свинка Пеппа'] == 1
+        assert collector.get_book_rating('Свинка Пеппа') == 1, "Рейтинг книги должен быть 1"
+
+    def test_set_book_rating_7(self):
+        collector = BooksCollector()
+        collector.add_new_book('Жизнь взаймы')
+        collector.set_book_rating('Жизнь взаймы', 7)
+        assert 'Жизнь взаймы' in collector.get_books_with_specific_rating(7), '"Жизнь взаймы" в списке с рейтингом 7'
 
     def test_get_book_rating_not_in_list(self):
         collector = BooksCollector()
         collector.get_book_rating('Книга Келлс')
-        assert collector.books_rating.get('Книга Келлс') == None
+        assert collector.get_book_rating('Книга Келлс') == None, "У книги не из списка нет рейтинга"
 
     def test_add_book_in_favorites_add_one_book(self):
         collector = BooksCollector()
         collector.add_new_book('Тень и кость')
         collector.add_new_book('Шестерка воронов')
         collector.add_book_in_favorites('Тень и кость')
-        assert 'Тень и кость' in collector.get_list_of_favorites_books()
+        assert 'Тень и кость' in collector.get_list_of_favorites_books(), '"Тень и кость" есть в списке любимых книг'
 
     def test_delete_book_from_favorites_del_one_book(self):
         collector = BooksCollector()
         collector.add_new_book('Шестерка воронов')
         collector.add_book_in_favorites('Шестерка Воронов')
         collector.delete_book_from_favorites('Шестерка Воронов')
-        assert len(collector.get_list_of_favorites_books()) == 0
+        assert len(collector.get_list_of_favorites_books()) == 0, "Список любимых книг пустой"
 
     def test_add_book_in_favorites_not_in_books_rating(self):
         collector = BooksCollector()
         collector.add_book_in_favorites('Разум и чувства')
-        assert len(collector.get_list_of_favorites_books()) == 0
+        assert len(collector.get_list_of_favorites_books()) == 0, "Список любимых книг пустой"
+
+    def test_get_books_with_specific_rating_book_with_rating_8(self):
+        collector = BooksCollector()
+        collector.add_new_book('Золушка')
+        collector.set_book_rating ('Золушка', 8)
+        collector.get_books_with_specific_rating(8)
+        assert len(collector.get_books_with_specific_rating(8)) == 1, "В списке 1 книга с рейтингом 8"
 
     def test_get_books_with_specific_rating_book_not_such_rating(self):
         collector = BooksCollector()
         collector.add_new_book('Тень и кость')
         collector.get_books_with_specific_rating(10)
-        assert len(collector.get_books_with_specific_rating(10))== 0
+        assert len(collector.get_books_with_specific_rating(10))== 0, "Список книг с ретингом 10 пустой"
